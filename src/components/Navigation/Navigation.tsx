e89ec6aa003455d0ccdcdf56453ab1194a131d87
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-03-21 16:18:48 +0530

    :zap: Completed the navbar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,10 +3,10 @@ import {Container, Nav, Navbar, NavbarBrand , NavLink} from "react-bootstrap";
 
 const Navigation : React.FC = () => {
     return(
-        <Navbar bg="dark" variant="dark" className="px-0 mx-0 py-3" sticky="top">
-            <Container className="mx-0 px-md-5 px-3 w-100">
-                <NavbarBrand href="/">Library App</NavbarBrand>
-                <Nav className=" ">
+        <Navbar bg="dark" variant="dark" className="px-0 mx-0 py-3 justify-content-center" sticky="top">
+            <Container className="mx-0 px-0">
+                <NavbarBrand href="/" className="">Library App</NavbarBrand>
+                <Nav className="">
                     <NavLink href="/" >Home</NavLink>
                     <NavLink href="/about" >About</NavLink>
                     <NavLink href="/contact" >Contact</NavLink>
